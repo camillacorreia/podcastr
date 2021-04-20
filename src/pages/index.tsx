46c37bc0e10311aa4227ffed1e983c992c2412d7
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-20 12:35:48 -0300

    feat: generating static home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
-export default function Home() {
+//import de API
+//SPA - useEffect (nao indexa no google)
+{/*
+useEffect(()=>{
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  },[]);
+*/}
+//SSR - getServerSideProps - faz a chamada sempre que entrar na página, mas só renderiza depois que tiver os dados e indexa ao google
+//SSG - getStaticProps - só funciona em produção, consegue setar intervalos de chamadas ao back
+
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 *8,
+  } 
 }
